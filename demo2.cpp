13055f0a78dffbd69e29512e0f75eac9e1d65492
Pierre-Alexandre Goya <pierre.alexandre.goya@gmail.com>
2024-08-08 12:25:30 +0200

    WIP

--- demo2.cpp
+++ demo2.cpp
@@ -1,5 +1,4 @@
 #include "neo/term/attributes.hpp"
-#include "neo/term/button.hpp"
 #include "neo/term/context.hpp"
 #include "neo/term/field.hpp"
 #include "neo/term/frame.hpp"
@@ -15,7 +14,7 @@ main()
 
     terminal::frame welcome(term, terminal::attributes(0, 0, term.width() / 2, 15));
 
-    terminal::shape box1(term, terminal::attributes(0, 0, term.width(), term.height()), &welcome);
+    terminal::shape box1(term, terminal::attributes(1, 1, term.width() - 1, term.height() - 1), &welcome);
 
     terminal::field field(term,
                           terminal::attributes(0, 0, 20, 3, &box1.get_attributes(), anchor::center, anchor::center), 20,
